a011a9081e4234a25f609a9328e1317c0f2a1686
himani-s993 <himanis993@gmail.com>
2022-10-29 12:47:07 +0530

    Add Sample DataSet class and Operation class

---
+++ sorting/DataSet.java
@@ -0,0 +1,10 @@
+public class DataSet {
+    public static int[] getSampleArray() {
+        int[] arr = {
+            99, 44,  6,   2, 1,
+             5, 63, 87, 283, 4,
+             0
+        };
+        return arr;
+    }
+}
