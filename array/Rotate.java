a1a6b3deee4e5b484c2292f00eaba2e0ca73b5e5
himani-s993 <himanis993@gmail.com>
2023-02-23 22:56:00 +0530

    Rotate using reversing array

--- array/Rotate.java
+++ array/Rotate.java
@@ -2,8 +2,9 @@ public class Rotate {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5}; 
         int k = 2; // no. of rotation
-        rotateByK(arr, k);
-        // Util.printArray(arr);
+        // rotateByK(arr, k);
+        rotateByKMethod2(arr, k);
+        Util.printArray(arr);
     }
 
     public static void rotateByK(int[] arr, int k) {
@@ -17,6 +18,33 @@ public class Rotate {
                 // print array after k elements
                 System.out.println(arr[i-k]);
             }
+        }
+    }
+
+    public static void rotateByKMethod2(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        // reverse k last elements
+        for (int i = n-k, j = n - 1; i < j; i++, j--) {
+           int temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp;
+        }
+
+        // reverse first n-k elements
+        for (int i = 0, j = n - k -1 ; i < j; i++) {
+           int temp = arr[i];
+           arr[i] = arr[j];
+           arr[j] = temp;
+           j--;
+        }
+
+         // reverse whole array
+        for (int i = 0, j = n - 1 ; i < j; i++) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j--;
         }
     }
 }
