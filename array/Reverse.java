030a71a1085de1e4ee53e1c6178e5aea414f4d44
himani-s993 <himanis993@gmail.com>
2023-02-23 22:21:39 +0530

    Reverse using recursion

--- array/Reverse.java
+++ array/Reverse.java
@@ -1,7 +1,8 @@
 public class Reverse {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4};
-        iterative(arr);
+        // iterative(arr);
+        recurive(arr, 0, arr.length - 1);
         Util.printArray(arr);
     }
 
@@ -13,5 +14,17 @@ public class Reverse {
             arr[j] = temp;
             j--;
         }
+    }
+
+    public static void recurive(int[] arr, int start, int end) {
+        
+        if (start >= end) {
+            return;
+        }
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        recurive(arr, start+1, end-1);
     }
 }
