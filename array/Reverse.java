f3ea6e728feaa27aec8a15cdc73ea08e8ad53753
himani-s993 <himanis993@gmail.com>
2023-02-23 22:15:17 +0530

    Refactoring

--- array/Reverse.java
+++ array/Reverse.java
@@ -1,6 +1,11 @@
 public class Reverse {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3};
+        int arr[] = {1, 2, 3, 4};
+        iterative(arr);
+        Util.printArray(arr);
+    }
+
+    public static void iterative(int[] arr) {
         int n = arr.length;
         for (int i = 0, j= n-1; i < n /2; i++) {
             int temp = arr[i];
@@ -8,6 +13,5 @@ public class Reverse {
             arr[j] = temp;
             j--;
         }
-        Util.printArray(arr);
     }
 }
