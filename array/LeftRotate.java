82ce191386d5cb46dd70b620eaaeae2dcfc67324
himani-s993 <himanis993@gmail.com>
2023-03-08 15:38:50 +0530

    Left rotate

---
+++ array/LeftRotate.java
@@ -0,0 +1,9 @@
+public class LeftRotate {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5}; 
+        int k = 2; // no. of rotation
+        // rotateByK(arr, k);
+        // rotateByKMethod2(arr, k);
+        Util.printArray(arr);
+    }
+}
