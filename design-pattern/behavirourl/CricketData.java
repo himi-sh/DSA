0e1239e37d28ed7b5c514af2a0ee6dd2919579c6
himani-s993 <himanis993@gmail.com>
2023-03-08 16:26:15 +0530

    DP -> behaviroual -> Observable DP

---
+++ design-pattern/behavirourl/CricketData.java
@@ -0,0 +1,17 @@
+public class CricketData {
+    int runs, wickets;
+    float overs;
+    AverageScore averageScore;
+
+    CurrentData(AverageScore averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    // getLatestRuns
+    // getLatestwicket
+
+    public void updateData() {
+        // upate the Average score data from here
+        
+    }
+}
